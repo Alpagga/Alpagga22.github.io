b0638a6a95197838e3429ecc333368f621779b91
Alpagga <lukas.schmid.01@icloud.com>
2024-09-13 22:32:02 +0200

    sketch1

--- P5.js/sketch1.js
+++ P5.js/sketch1.js
@@ -1,8 +1,15 @@
 function sketch1(p) {
+
+    let x = 25;
+    let y = 50;
+
     p.setup = function() {
-        p.createCanvas(p.windowWidth, p.windowHeight); 
-        p.background(200);
-        p.ellipse(p.width / 2, p.height / 2, 100, 100);
+        p.createCanvas(p.windowWidth, p.windowHeight);    
+ 
+
+        p.textSize(20);
+        p.colorMode(p.HSB);
+        p.loop();
     };
 
     p.windowResized = function() {
@@ -10,7 +17,23 @@ function sketch1(p) {
     };
 
     p.draw = function() {
-        // Your drawing logic for sketch1
+        // Clear the background
+        p.background(0);
+
+        // Draw a circle, with hue determined by frameCount
+        p.fill(x / 3, 90, 90);
+        p.circle(x, p.windowHeight / 2, y);
+
+        // Increase the x variable by 5
+        x += 5;
+        y += 1;
+
+        // Reset the circle position after it moves off the right side
+        if (x > p.windowWidth + 150) {
+            x = -50;
+            y = 50;
+        }
+
     };
 }
 
--- P5.js/sketch2.js
+++ P5.js/sketch2.js
@@ -2,7 +2,7 @@ function sketch2(p) {
   p.setup = function() {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.background(200);
-      p.line(200, 200, 100, 100);
+    p.line(200, 200, 100, 100);
   };
 
   p.windowResized = function() {
